22e01a3cbca0a27a32690e72e9d65bebcdcbb99d
Victor Anogwih <anogwihvictor@gmail.com>
2024-04-05 00:41:17 -0400

    Style applied to photo component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-// import "../styles/PhotoListItem.scss";
+import "../styles/PhotoListItem.css";
 import photos from "mocks/photos";
 
 
 
 
 const PhotoListItem = ({ photo }) => {
-  const { id, location, imageSource, username, profile } = photo;
+  const { location, imageSource, username, profile } = photo;
   return (
     <div>
+      <img className="photo-list__item" src={imageSource} alt="Photo" />
       <img src={profile} alt="Profile" />
       <div>
         <div>
           <span>{location.city},</span>
           <span>{location.country}</span>
         </div>
-        <img src={imageSource} alt="Photo" />
         <div>{username}</div>
       </div>
     </div>
