7583e27e2987046ec5d1ae8e7e50faf79b9ab575
Victor Anogwih <anogwihvictor@gmail.com>
2024-04-05 02:01:06 -0400

    displaying 3 different photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,12 +18,13 @@ const sampleDataForPhotoListItem = {
   
 };
   
-
 const App = () => {
+  // Create an array with a length of 3
   const photos = new Array(3).fill(null);
 
   return (
     <div className="App">
+      {/* Map over the array and render a PhotoListItem component for each item */}
       {photos.map((_, index) => (
         <PhotoListItem key={index} photo={sampleDataForPhotoListItem} />
       ))}
