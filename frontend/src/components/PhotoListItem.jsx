58cced9d2e0805c6e29ad37bdf59bd6861376c09
Victor Anogwih <anogwihvictor@gmail.com>
2024-04-05 00:50:57 -0400

    Style applied to photo component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,16 +8,17 @@ import photos from "mocks/photos";
 const PhotoListItem = ({ photo }) => {
   const { location, imageSource, username, profile } = photo;
   return (
-    <div>
-      <img className="photo-list__item" src={imageSource} alt="Photo" />
-      <img src={profile} alt="Profile" />
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={imageSource} alt="Photo"/>
       <div>
-        <div>
+        <img className="photo-list__user-profile" src={profile} alt="Profile"/>
+        <div className="photo-list__user-info">
           <span>{location.city},</span>
           <span>{location.country}</span>
         </div>
-        <div>{username}</div>
+        <div className="photo-list__user-info">{username}</div>
       </div>
+      
     </div>
   );
 };
