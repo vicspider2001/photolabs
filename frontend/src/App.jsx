66de820ca67d74f3b43b04a8e82bcdeb23b5e0de
Victor Anogwih <anogwihvictor@gmail.com>
2024-04-05 01:58:44 -0400

    displaying 3 different photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import PhotoListItem from './components/PhotoListItem';
 // import './App.scss';
 
 
-const sampleDataForPhotoListItem = [
-  {
+const sampleDataForPhotoListItem = {
+  
     id: "1",
   location: {
     city: "Montreal",
@@ -14,48 +14,21 @@ const sampleDataForPhotoListItem = [
   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
   username: "Joe Example",
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
   
-  {
-    id: "2",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-
-  {
-    id: "3",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-
-]
   
-
-
+};
+  
 
 const App = () => {
-  
-    return (
-      <div className="App">
-        
-        {sampleDataForPhotoListItem.map((photoData) => (
-          <PhotoListItem key={photoData.id} photo={photoData} />
-        ))}
-          
-      </div>
-    );
-  
-  
+  const photos = new Array(3).fill(null);
+
+  return (
+    <div className="App">
+      {photos.map((_, index) => (
+        <PhotoListItem key={index} photo={sampleDataForPhotoListItem} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
