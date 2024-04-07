0181ab278e69a7cfb5f305ea1e8338fc378a4efe
Victor Anogwih <anogwihvictor@gmail.com>
2024-04-07 19:38:41 -0400

    Mock Data Added in code refactor

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,31 +1,13 @@
 import React from "react";
 import TopicListItem from "./TopicListItem";
-
+import topics from "mocks/topics";
 import "../styles/TopicList.scss";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topic) => (
+      {topics.map((topic) => (
         <TopicListItem key={topic.id} topic={topic} />
       ))}
     </div>
