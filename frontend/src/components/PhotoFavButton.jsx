f5664db6feaa2fa71fc7ec4b09848981b5e7fe29
Victor Anogwih <anogwihvictor@gmail.com>
2024-04-07 22:41:00 -0400

    Stored favourited Photos

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,18 +1,13 @@
-import React, { useState } from 'react';
+// PhotoFavButton.jsx
+import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
-  const [selected, setSelected] = useState(false);
-   
-   const handleClick = ({isFav, onClick}) => {
-    setSelected(prevSelected => !prevSelected);
-  };
-
+function PhotoFavButton({ isFav, onClick }) {
   return (
-    <div className="photo-list__fav-icon ${isFav ? 'selected' : ''}`} onClick={onClick}" onClick={handleClick}>
+    <div className={`photo-list__fav-icon ${isFav ? 'selected' : ''}`} onClick={onClick}>
       <div className="photo-list__fav-icon-svg">
-        <FavIcon selected={selected} />
+        <FavIcon selected={isFav} />
       </div>
     </div>
   );
