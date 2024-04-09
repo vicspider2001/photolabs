ac1dbc52315eea00469bdb6d0f231d1ca3a10ead
Victor Anogwih <anogwihvictor@gmail.com>
2024-04-09 07:52:00 -0400

    blank modal added

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ onClose }) => {
   return (
     <div className="photo-details-modal">
+      {/* Modal content goes here */}
       <button className="photo-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol" />
       </button>
